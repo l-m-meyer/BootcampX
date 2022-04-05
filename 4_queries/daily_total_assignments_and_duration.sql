79d23c97b33891099dfc1fe0455f4c6c0f959cf6
Lisa Meyer <lisamermey@gmail.com>
2022-04-05 18:33:14 -0400

    Add queries to assignment

---
+++ 4_queries/daily_total_assignments_and_duration.sql
@@ -0,0 +1,6 @@
+SELECT  day,
+        COUNT(*) as number_of_assignments,
+        SUM(duration) as duration
+  FROM assignments
+  GROUP BY day
+  ORDER BY day;
---
+++ 4_queries/teachers_who_assisted_cohort.sql
@@ -0,0 +1,17 @@
+SELECT  DISTINCT teachers.name as teacher,
+        cohorts.name as cohort
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = 'JUL02'
+  ORDER BY teacher;
+
+-- COMPASS ANSWER
+-- SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+-- FROM teachers <-- this is where it differed
+-- JOIN assistance_requests ON teacher_id = teachers.id
+-- JOIN students ON student_id = students.id
+-- JOIN cohorts ON cohort_id = cohorts.id
+-- WHERE cohorts.name = 'JUL02'
+-- ORDER BY teacher;
