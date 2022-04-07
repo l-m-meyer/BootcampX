69e459d0a9131069cacbabecd6c1fb7c664bc92a
Lisa Meyer <lisamermey@gmail.com>
2022-04-06 21:17:18 -0400

    Add parameterized queries to teachers/students

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,16 +8,21 @@ const pool = new Pool({
 });
 
 const args = process.argv.slice(2);
-const month = args[0];
-const limit = args[1];
+const cohortName = args[0];
+const limit = args[1] || 5;
 
-pool.query(`
-  SELECT students.id, students.name, cohorts.name as cohort
-    FROM students
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '${month}%'
-    LIMIT ${limit || 5};
-`)
+// store all potentially malicious values in an array
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort.`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,20 @@ const pool = new Pool({
 });
 
 const args = process.argv.slice(2);
-const month = args[0];
+const month = args[0] || 'JUL02';
 
-pool.query(`
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-    FROM teachers
-    JOIN assistance_requests ON teachers.id = teacher_id
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name = '${month || 'JUL02'}'
-    ORDER BY teacher;
-`)
+const values = [`${month}`];
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
